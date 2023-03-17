0790f7455dab3364c08e0d6adb8caf1baaca2a98
mattttyyyy <wehttamocsalb@gmail.com>
2023-03-17 13:44:09 +0800

    feat: boilerplate vite react init config

---
+++ src/data/conversations-data.js
@@ -0,0 +1,29 @@
+export const conversationsData = [
+  {
+    img: "/website/assets/team-1.jpeg",
+    name: "Sophie B.",
+    message: "Hi! I need more information...",
+  },
+  {
+    img: "/website/assets/team-2.jpeg",
+    name: "Alexander",
+    message: "Awesome work, can you...",
+  },
+  {
+    img: "/website/assets/team-3.jpeg",
+    name: "Ivanna",
+    message: "About files I can...",
+  },
+  {
+    img: "/website/assets/team-4.jpeg",
+    name: "Peterson",
+    message: "Have a great afternoon...",
+  },
+  {
+    img: "/website/assets/bruce-mars.jpeg",
+    name: "Bruce Mars",
+    message: "Hi! I need more information...",
+  },
+];
+
+export default conversationsData;
---
+++ src/data/projects-table-data.js
@@ -0,0 +1,65 @@
+export const projectsTableData = [
+  {
+    img: "/website/assets/logo-xd.svg",
+    name: "Material XD Version",
+    members: [
+      { img: "/website/assets/team-1.jpeg", name: "Romina Hadid" },
+      { img: "/website/assets/team-2.jpeg", name: "Ryan Tompson" },
+      { img: "/website/assets/team-3.jpeg", name: "Jessica Doe" },
+      { img: "/website/assets/team-4.jpeg", name: "Alexander Smith" },
+    ],
+    budget: "$14,000",
+    completion: 60,
+  },
+  {
+    img: "/website/assets/logo-atlassian.svg",
+    name: "Add Progress Track",
+    members: [
+      { img: "/website/assets/team-2.jpeg", name: "Ryan Tompson" },
+      { img: "/website/assets/team-4.jpeg", name: "Alexander Smith" },
+    ],
+    budget: "$3,000",
+    completion: 10,
+  },
+  {
+    img: "/website/assets/logo-slack.svg",
+    name: "Fix Platform Errors",
+    members: [
+      { img: "/website/assets/team-3.jpeg", name: "Jessica Doe" },
+      { img: "/website/assets/team-1.jpeg", name: "Romina Hadid" },
+    ],
+    budget: "Not set",
+    completion: 100,
+  },
+  {
+    img: "/website/assets/logo-spotify.svg",
+    name: "Launch our Mobile App",
+    members: [
+      { img: "/website/assets/team-4.jpeg", name: "Alexander Smith" },
+      { img: "/website/assets/team-3.jpeg", name: "Jessica Doe" },
+      { img: "/website/assets/team-2.jpeg", name: "Ryan Tompson" },
+      { img: "/website/assets/team-1.jpeg", name: "Romina Hadid" },
+    ],
+    budget: "$20,500",
+    completion: 100,
+  },
+  {
+    img: "/website/assets/logo-jira.svg",
+    name: "Add the New Pricing Page",
+    members: [{ img: "/website/assets/team-4.jpeg", name: "Alexander Smith" }],
+    budget: "$500",
+    completion: 25,
+  },
+  {
+    img: "/website/assets/logo-invision.svg",
+    name: "Redesign New Online Shop",
+    members: [
+      { img: "/website/assets/team-1.jpeg", name: "Romina Hadid" },
+      { img: "/website/assets/team-4.jpeg", name: "Alexander Smith" },
+    ],
+    budget: "$2,000",
+    completion: 40,
+  },
+];
+
+export default projectsTableData;
