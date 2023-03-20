04a9d549030c7d3c4791ed723305b383f3a0e415
mattttyyyy <wehttamocsalb@gmail.com>
2023-03-20 15:33:01 +0800

    feat: sitehero 1200 responsive development

--- src/pages/sitehome/sitehero.jsx
+++ src/pages/sitehome/sitehero.jsx
@@ -6,30 +6,32 @@ export function Sitehero() {
     return (
         <React.Fragment>
         <div className="hero-wrap">
-            <div className="hero-left-content">
-                <div className="hero-left-content-wrap">
-                    <div className="hero-left-content-title">
-                        {heroData.left_title}
-                    </div>
-                    <div className="hero-left-content-desc">
-                        {heroData.left_desc}
-                    </div>
-                    <div className="hero-left-content-action-items">
-                        <button className="hero-left-content-action-item-call">{heroData.left_call}</button>
-                        <div className="hero-left-content-action-item-play">
-                            <div className="play-wrap">
-                                <div className="play-wrap-shape">
+            <div className="hero-container">
+                <div className="hero-left-content">
+                    <div className="hero-left-content-wrap">
+                        <div className="hero-left-content-title">
+                            {heroData.left_title}
+                        </div>
+                        <div className="hero-left-content-desc">
+                            {heroData.left_desc}
+                        </div>
+                        <div className="hero-left-content-action-items">
+                            <button className="hero-left-content-action-item-call">{heroData.left_call}</button>
+                            <div className="hero-left-content-action-item-play">
+                                <div className="play-wrap">
+                                    <div className="play-wrap-shape">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="hero-left-content-action-item-watch">
-                            {heroData.left_watch}
+                            <div className="hero-left-content-action-item-watch">
+                                {heroData.left_watch}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="hero-right-content">
-                <img className="hero-right-content-img" src="/website/assets/bv-asset/bv-header-media.png"></img>
+                <div className="hero-right-content">
+                    <img className="hero-right-content-img" src="/website/assets/bv-asset/bv-header-media.png"></img>
+                </div>
             </div>
         </div>
         </React.Fragment>
