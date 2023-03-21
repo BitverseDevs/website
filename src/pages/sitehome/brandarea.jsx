abc5d184ebb9fbb3bad0918da860034a7d6bc7d3
mattttyyyy <wehttamocsalb@gmail.com>
2023-03-21 13:53:26 +0800

    feat: brandArea code split optimize and init sectionTitle data

--- src/pages/sitehome/brandarea.jsx
+++ src/pages/sitehome/brandarea.jsx
@@ -1,51 +1,40 @@
-import React, { useState, useEffect, useRef } from "react";
-import { brandArea } from "./data/brandarea";
+import React from "react";
+import SectionTitle from "./components/sectionTitle";
+import { responsiveBrands, sectionTitles } from "./data/responsivebrands";
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import "./brandarea.scss";
 
-export function BrandArea(brands) {
-    const brandArray = Object.values(brands.brands);
+
+
+export function BrandArea(props) {
+    const brandArray = Object.values(props.brands);
+    const { featMsg, title, desc } = sectionTitles[0];
     const handleDragStart = (e) => e.preventDefault();
-    const responsive = {
-        0: {
-            items: 2,
-        },
-        575:{
-            items: 3,
-        },
-        767:{
-            items: 4,
-        },
-        991:{
-            items: 5,
-        },
-        1200:{
-            items: 6,
-        }
-    };
     const items2 = brandArray.map(({id, src}) => (
         <img src={src} key={id} onDragStart={handleDragStart} role='presentation'
         >
         </img>
-    ))
+    ));
   return (
-    <div className="carousel-wrap">
-        <AliceCarousel 
-        mouseTracking
-        // autoWidth
-        // autoHeight
-        autoPlay
-        infinite
-        autoPlayInterval = {4500}
-        animationDuration = {1000}
-        disableDotsControls
-        disableButtonsControls 
-        items={items2} 
-        responsive={responsive}
-        />
-    </div>
-    
+    <React.Fragment>
+        <SectionTitle featMsg={featMsg} title={title} desc={desc} marginBottom={'40'}/>
+        <div className="carousel-wrap">
+            <AliceCarousel 
+            mouseTracking
+            // autoWidth
+            // autoHeight
+            autoPlay
+            infinite
+            autoPlayInterval = {4500}
+            animationDuration = {1000}
+            disableDotsControls
+            disableButtonsControls 
+            items={items2} 
+            responsive={responsiveBrands}
+            />
+        </div>
+    </React.Fragment>
   );
 }
 
