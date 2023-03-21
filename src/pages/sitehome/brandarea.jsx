0d099c0204cb7b94d40edd5e72a132336ef52cf5
mattttyyyy <wehttamocsalb@gmail.com>
2023-03-21 11:48:49 +0800

    feat: brand area carousel add dependency and implement development

--- src/pages/sitehome/brandarea.jsx
+++ src/pages/sitehome/brandarea.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { brandArea } from "./data/brandarea";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import "./brandarea.scss";
-
 
 export function BrandArea(brands) {
     const brandArray = Object.values(brands.brands);
-    return (
-        <React.Fragment>
-        <div className="brand-area-wrap">
-        {brandArray.map(({ id, src })=>
-            (
-            <div className="brand-area-item" key={id}>
-                <img src={src}></img>
-            </div>
-        )    
-        )}
-        </div>
-        </React.Fragment>
-    );
+    const handleDragStart = (e) => e.preventDefault();
+    const responsive = {
+        0: {
+            items: 2,
+        },
+        575:{
+            items: 3,
+        },
+        767:{
+            items: 4,
+        },
+        991:{
+            items: 5,
+        },
+        1200:{
+            items: 6,
+        }
+    };
+    const items2 = brandArray.map(({id, src}) => (
+        <img src={src} key={id} onDragStart={handleDragStart} role='presentation'
+        >
+        </img>
+    ))
+  return (
+    <div className="carousel-wrap">
+        <AliceCarousel 
+        mouseTracking
+        // autoWidth
+        // autoHeight
+        autoPlay
+        infinite
+        autoPlayInterval = {4500}
+        animationDuration = {1000}
+        disableDotsControls
+        disableButtonsControls 
+        items={items2} 
+        responsive={responsive}
+        />
+    </div>
+    
+  );
 }
-
 
 export default BrandArea;
