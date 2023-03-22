4ca7fcda38fc8e43b9b4b6d753a8fb8f2f52bd66
mattttyyyy <wehttamocsalb@gmail.com>
2023-03-22 16:07:39 +0800

    feat: featuredspecs component completion dev

--- src/pages/sitehome/featuredSpecs.jsx
+++ src/pages/sitehome/featuredSpecs.jsx
@@ -16,6 +16,7 @@ export function FeaturedSpecs(props) {
     const secondAsset = featuredSpecAssets[1]?.assetSrc;
     const { featMsg: featMsg1, title: title1, desc: desc1 } = sectionTitles[1];
     const { featMsg: featMsg2, title: title2, desc: desc2 } = sectionTitles[2];
+    const { featMsg: featMsg3, title: title3, desc: desc3 } = sectionTitles[3];
     const breakPoint = helpers.websiteBreakpoints(width);
     const breakpointRender = (key) => {
         const keyProcessor = {
@@ -36,10 +37,27 @@ export function FeaturedSpecs(props) {
         <React.Fragment>
             <div className="featured-specs-wrap">
                 <SectionTitle featMsg={featMsg1} title={title1} desc={desc1} marginBottom={'75'}/>
-                <div className='featured-specs-cont1' border-render={`${isRender}`}>
-                    <img className='featured-specs-img1' src={firstAsset} alt={featuredSpecAssets[0]?.alt}/>
+                <div className='featured-specs-cont' border-render={`${isRender}`}>
+                    <figure className='featured-specs-img-wrap'>
+                        <img className='featured-specs-img1' src={firstAsset} alt={featuredSpecAssets[0]?.alt}/>
+                    </figure>
                     {isRender ? <BorderLine/> : null}
                     <SectionTitleV2 featMsg={featMsg2} title={title2} desc={desc2} isDescUnorderedListed={true}/>
+                </div>
+                <div className='featured-specs-cont featured-specs-cont2' border-render={`${isRender}`}>
+                    {isRender ?
+                    <>
+                    <div className='featured-specs-cont-title-wrap'>
+                        <SectionTitleV2 featMsg={featMsg3} title={title3} desc={desc3} isDescUnorderedListed={false}/>
+                    </div>
+                    <BorderLine/>
+                    </> 
+                    : 
+                    <SectionTitleV2 featMsg={featMsg3} title={title3} desc={desc3} isDescUnorderedListed={false}/>
+                    }
+                    <figure className='featured-specs-img-wrap'>
+                        <img className='featured-specs-img2' src={secondAsset} alt={featuredSpecAssets[1]?.alt}/>
+                    </figure>
                 </div>
             </div>
         </React.Fragment>
