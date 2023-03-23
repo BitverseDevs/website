11d8576285d532cf1b686785ef4a9a62372713d7
mattttyyyy <wehttamocsalb@gmail.com>
2023-03-23 18:32:51 +0800

    fix: buildError

--- src/pages/sitehome/productcarousel/productcarousel.jsx
+++ src/pages/sitehome/productcarousel/productcarousel.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import './productcarousel.scss';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import { responsiveBrands, productCarouselHome } from '../data/siteHome';
+import { responsiveBrands, productCarouselHome } from '../data/sitehome';
 // import ProductItems from './product-items/productitems';
 import SectionTitleV2 from '../components/sectiontitleV2/sectiontitlev2';
 import useWindowDimensions from '@/custom-hooks/use-window-dimension/use-window-dimension';
