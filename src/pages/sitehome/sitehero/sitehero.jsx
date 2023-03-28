7b024c392a5af878f5660425935b32fc11123c38
mattttyyyy <wehttamocsalb@gmail.com>
2023-03-28 16:17:16 +0800

    feat: sitehero embed video popup development

--- src/pages/sitehome/sitehero/sitehero.jsx
+++ src/pages/sitehome/sitehero/sitehero.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sitehero.scss';
 import { heroData } from '../data/sitehome';
 
 export function Sitehero() {
+    const [selectedItem, setSelectedItem] = useState(false); 
+    function handleClick() {
+        setSelectedItem(!selectedItem);
+      }
     return (
         <React.Fragment>
         <section className='hero-wrap'>
@@ -17,10 +21,10 @@ export function Sitehero() {
                         </div>
                         <div className='hero-left-content-action-items'>
                             <button className='hero-left-content-action-item-call'>{heroData.left_call}</button>
-                            <div className='hero-left-content-action-item-play'>
+                            <div className='hero-left-content-action-item-play' onClick={handleClick}>
                                 <div className='play-wrap'>
-                                    <div className='play-wrap-shape'>
-                                    </div>
+                                    <button className='play-wrap-shape'>
+                                    </button>
                                 </div>
                             </div>
                             <div className='hero-left-content-action-item-watch'>
@@ -34,6 +38,10 @@ export function Sitehero() {
                 </div>
             </div>
         </section>
+        <section className={selectedItem ? 'overlay_active': ''} onClick={handleClick}>
+        </section>
+        {/* <iframe className={selectedItem ? 'iframe_active' : 'iframe_inactive'} src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F100090029121157%2Fvideos%2F714203046926001%2F&show_text=false&width=560&t=0" width="560" height="314" style={{border: 'none', overflow: 'hidden'}} scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe> */}
+        <iframe className={selectedItem ? 'iframe_active' : 'iframe_inactive'} src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F100090029121157%2Fvideos%2F714203046926001%2F&show_text=false&width=560&t=0" style={{overflow: 'hidden'}} allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </React.Fragment>
     );
 }
